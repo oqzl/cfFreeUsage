3e55885d58bf319a47ffbecca0a59c3cdf3891cb
Kenji Ishikawa <ke.oqzl@gmail.com>
2026-09-10 10:23:14 +0900

    Remove obsolete public PWA source

--- public/config.js
+++
@@ -1,7 +0,0 @@
-export const OAUTH = {
-  clientId: "b46be3b9dbc67bb0fc81c09d7b3734a3",
-  scopes: ["account-settings.read", "account-analytics.read"],
-  authorizationEndpoint: "https://dash.cloudflare.com/oauth2/auth",
-  tokenEndpoint: "https://dash.cloudflare.com/oauth2/token",
-  revokeEndpoint: "https://dash.cloudflare.com/oauth2/revoke"
-};

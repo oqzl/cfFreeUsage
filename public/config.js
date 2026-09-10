cb9104bad7cc22520fe216f1698c44d3dcdd319e
Kenji Ishikawa <ke.oqzl@gmail.com>
2026-09-10 09:59:53 +0900

    OAuth Client ID を追記

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 export const OAUTH = {
-  clientId: "PASTE_CLOUDFLARE_OAUTH_CLIENT_ID",
+  clientId: "b46be3b9dbc67bb0fc81c09d7b3734a3",
   scopes: ["account-settings.read", "account-analytics.read"],
   authorizationEndpoint: "https://dash.cloudflare.com/oauth2/auth",
   tokenEndpoint: "https://dash.cloudflare.com/oauth2/token",
